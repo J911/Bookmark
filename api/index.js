759c63f071d5f2453f4392b2829ae0f02b86ce16
J911 <fb.jaemin@gmail.com>
2018-03-18 14:59:56 +0900

    bug fixed!

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getBookmark } = require('modules')
+const { getBookmark } = require('./modules')
 
 router.get('/Bookmark', (req, res)=> getBookmark(req, res));
 
