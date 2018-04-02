f6295ba49b3dd96b6aa1691585aefe4d3ff10a27
J911 <fb.jaemin@gmail.com>
2018-04-03 08:59:42 +0900

    update express-session

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const session = require('express-session');
+
+app.use(session({
+ secret: '!@#SECURE@#$',
+ resave: false,
+ saveUninitialized: true
+}));
+
 app.use('/v1', api);
 
 app.listen(3000);
