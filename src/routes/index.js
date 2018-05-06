18bcc1fda64930e85f9009cc491da1d2fa9d8d95
Jay <fb.jaemin@gmail.com>
2018-05-06 15:56:54 +0900

    remove user router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router()
 const auth = require('./auth')
 const bookmark = require('./bookmark')
-const user = require('./user')
 
 router.use('/auth', auth)
 router.use('/bookmark', bookmark)
